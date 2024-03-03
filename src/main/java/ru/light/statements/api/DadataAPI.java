c5f709b765ba0a2ac9c8625aa61190eb980228e3
DemonS2803 <101480871+DemonS2803@users.noreply.github.com>
2024-03-03 20:31:07 +0300

    Update DadataAPI.java

--- src/main/java/ru/light/statements/api/DadataAPI.java
+++ src/main/java/ru/light/statements/api/DadataAPI.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 @FeignClient(name = "dadata-client", url = "https://cleaner.dadata.ru/api/v1")
 public interface DadataAPI {
 
-    // я правда хотел вынести это в конфиги, но потом вспомнил, что все равно в гит выгружать (локально сохранять - неудобно проверять) 
+    // я правда хотел вынести это в конфиги, но потом вспомнил, что все равно в гит выгружать (локально сохранять в конфиги - неудобно проверять) 
     public static final String dadataToken = "Token 31f00a4cf553cc43ac8b7596b40ed8c2e641b67c";
     public static final String dadataSecret = "b960b9842e9e669ded6d01231a05b73634c1ec97";
 
